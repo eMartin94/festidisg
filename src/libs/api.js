5bc439dbba702c651749f0feb50569eda0901d64
Martin Pizango <eduardo.martin688@gmail.com>
2023-11-30 01:14:11 -0500

    update

--- src/libs/api.js
+++ src/libs/api.js
@@ -14,6 +14,7 @@ export const sendContactForm = async (data) => {
     }
     return response;
   } catch (error) {
+    console.log(error);
     throw new Error('Error de red al enviar el formulario');
   }
 };
